8c6dd52c48ea9950b32b0513dd9481c91f1ecb62
charlesaraya <charlesaraya@gmail.com>
2025-04-15 01:10:52 +0200

    test: add unit tests

---
+++ lists/test_list.c
@@ -0,0 +1,28 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+int main() {
+    list_t list;
+    const int INITIAL_CAPACITY = 10;
+    printf("Test init list...\n");
+    init_list(&list, TYPE_INT, INITIAL_CAPACITY);
+    assert(list.capacity == INITIAL_CAPACITY);
+    assert(list.length == 0);
+    assert(list.type_size == sizeof(int));
+    assert(list.data != NULL);
+
+    printf("Test append...\n");
+    for (int i = 0; i < INITIAL_CAPACITY + 1; i++) {
+        append(&list, &i);
+    }
+    assert(list.capacity == INITIAL_CAPACITY * 2);
+    assert(list.length == INITIAL_CAPACITY + 1);
+    assert(list.data != NULL);
+
+    free(list.data);
+
+    printf("All tests passed.\n");
+    return 0;
+}
